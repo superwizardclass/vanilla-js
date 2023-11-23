b2a1d75121eeb7643535808f2b8ad9640188cc0a
superwizardclass <wizardclassio@gmail.com>
2023-11-22 18:06:57 -0800

    Built generic form to see the difference in sending data from HTML and javascript.

--- form/src/main.js
+++ form/src/main.js
@@ -1,7 +1,15 @@
-const form = document.getElementById("cuttlefishForm");
+(function () {
+  const form = document.getElementById("cuttlefishForm");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const formData = new FormData(form);
-  console.log([...formData]);
-});
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    console.log([...formData]);
+
+    const cuttlefishList = document.getElementById("cuttlefishList");
+    console.log(cuttlefishList);
+    const newRow = document.createElement("div");
+    newRow.appendChild(document.createTextNode(`Name:`));
+    cuttlefishList.appendChild(newRow);
+  });
+})();
